f9cef72dcbb1f4def531884e9de8badb040d8596
Melissa Rodriguez <olenellina@gmail.com>
2016-11-04 10:19:23 -0700

    additional controller tests working

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
 class RecipesControllerTest < ActionController::TestCase
-  # test "should get index" do
-  #   get :index
-  #   assert_response :success
-  # end
-  #
-  test "should get show" do
+  test "should get index for a valid query" do
+    get :index, {query: "tacos"}
+    assert_response :success
+  end
+
+  test "should get show for a recipe record" do
     get :show, {id: 1}
     assert_response :success
   end
@@ -14,7 +14,11 @@ class RecipesControllerTest < ActionController::TestCase
   test "should get search" do
     get :search
     assert_response :success
-    assert_render :index
+  end
+
+  test "should get a flash notice if query didn't produce results from API call" do
+    get :index, {query: "2093840q-9"}
+    assert_equal flash[:notice], "Your search failed to return results. Please try again"
   end
 
 end
